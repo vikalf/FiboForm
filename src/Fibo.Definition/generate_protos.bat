16195dcac71febad100a78c3df69892d18e743b4
Valonso <Valonso@kaplan.edu>
2019-12-21 15:56:02 -0600

    Initial Commit

---
+++ src/Fibo.Definition/generate_protos.bat
@@ -0,0 +1,3 @@
+﻿..\..\..\..\gRPC\tools\2.22.0\protoc.exe -I protos --csharp_out .\ --grpc_out .\ protos\fibo.proto --plugin=protoc-gen-grpc=..\..\..\..\gRPC\tools\1.22.0\grpc_csharp_plugin.exe
+
+@rem grpcc --proto fibo.proto -i --address 127.0.0.1:50051
